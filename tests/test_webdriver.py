3b3ec9fc267ebe1ded1bb549a8d69205617b28de
Kevin Watt <kevinwatt@kpmg.com>
2020-06-05 12:40:36 -0500

    Updated chromedriver path

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -1,12 +1,15 @@
 # Project imports
 from icecreamscrape.webdriver import driver
 
+import os
+
 
 def test_init_driver(webdriver):
 	driver = webdriver.driver
 	try:
 		driver.get("http://google.com")
 	except:
-		assert 1 == 0, driver
+		
+		assert 1 == 0, os.path_exists(os.path.join(driver, "chromedriver"))
 	assert driver.title == "Google", driver
 
