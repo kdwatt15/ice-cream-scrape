a3f502b4745a028413b836d8374e59722665781c
Kevin Watt <kevinwatt@kpmg.com>
2020-06-05 12:33:39 -0500

    Updated assert response

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -4,6 +4,9 @@ from icecreamscrape.webdriver import driver
 
 def test_init_driver(webdriver):
 	driver = webdriver.driver
-	driver.get("http://google.com")
+	try:
+		driver.get("http://google.com")
+	except:
+		assert 1 == 0, driver
 	assert driver.title == "Google", driver
 
