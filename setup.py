f28a46e4d81e9b593e0fcc93d921ca2381b6f0e1
Kevin Watt <kevinwatt@kpmg.com>
2020-06-05 11:41:51 -0500

    lib -> Lib

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
-"""
+
 with open("README.md", "r") as f:
 	long_description = f.read()
 	f.close
-"""
+
 with open("requirements.txt", "r") as f:
 	install_requires = f.readlines()
 	f.close
@@ -14,7 +14,7 @@ setuptools.setup(
 	author="Kevin Watt",
 	author_email="kdwatt15@gmail.com",
 	description="CLI for bulk actions against Intralinks instances.",
-	#long_description=long_description,
+	long_description=long_description,
 	long_description_content_type="test/markdown",
 	packages=setuptools.find_packages(include=["icecreamscrape"]),
 	package_dir={'': 'Lib'},
